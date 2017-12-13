e49123306e5dddb5fab643f2d42c04e31fe71f19
Conner Charlebois <cdcharlebois@gmail.com>
2017-12-13 17:23:52 -0500

    Revert to 535b9183c5b0a8aa20d8612a776feee68f5220c1

--- src/www/scripts/app.js
+++ src/www/scripts/app.js
@@ -698,7 +698,6 @@ module.exports = (function() {
     };
 
     var credentialsProvided = function(username, password) {
-        console.log("checking credentials: [username:password] [" + username + ":" + password + "]");
         return (
             typeof username !== 'undefined' &&
             username.length > 0 &&
@@ -718,8 +717,6 @@ module.exports = (function() {
     //     settings.enableOffline, settings.persistentSession.enabled, settings.persistentSession.forceSecurity,
     //     userPin, userFingerprint, settings.username, settings.password, settings.updateAsync);
     var initialize = async function(url, enableOffline, persistentSessionEnabled, persistentSessionForceSecurity, pin, finger, username, password, updateAsync) {
-        console.log("initialize called with the following arguments:")
-        console.log(arguments);
         enableOffline = !!enableOffline;
 
         // Make sure the url always ends with a /
@@ -866,12 +863,9 @@ module.exports = (function() {
                 await cleanUpRemains();
             }
         } else if (persistentSessionForceSecurity) {
-            var loginUsername = document.getElementById("mx-username");
-            var loginPassword = document.getElementById("mx-password");
-            if (credentialsProvided(loginUsername.value, loginPassword.value)) {
-                await cleanUpRemains();
-                await createSessionWithCredentials(appUrl, loginUsername.value, loginPassword.value);
-            }
+            // require either fingerprint of pin for a persistent session
+            await logout();
+            await cleanUpRemains();
         } else {
             // something?
         }
